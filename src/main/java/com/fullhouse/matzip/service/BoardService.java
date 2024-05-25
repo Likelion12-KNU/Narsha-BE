42c2d11dcfc884b5f2a6ceade9f00cfcae543874
yuhano <30584359+yuhano@users.noreply.github.com>
2024-05-26 06:14:45 +0900

    [fix] merge 과정에서 잘못 합쳐진 부분 삭제

--- src/main/java/com/fullhouse/matzip/service/BoardService.java
+++ src/main/java/com/fullhouse/matzip/service/BoardService.java
@@ -90,12 +90,4 @@ public class BoardService {
         boardRepository.delete(board);
     }
 
-    public Board createBoard(Board board){
-        return boardRepository.save(board);
-    }
-
-    public void deleteBoard(Long id){
-        Board board = boardRepository.findById(id).orElseThrow(() -> new RuntimeException("Post not found with id " + id));
-        boardRepository.delete(board);
-    }
 }
