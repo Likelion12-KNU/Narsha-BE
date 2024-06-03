7c83c4038fbcae8aa6870e18c846e6e4a631dbb3
chopin-Op-23 <137890694+chopin-Op-23@users.noreply.github.com>
2024-06-03 23:29:09 +0900

    Update Board.java

--- src/main/java/com/fullhouse/matzip/model/Board.java
+++ src/main/java/com/fullhouse/matzip/model/Board.java
@@ -1,23 +1,51 @@
 package com.fullhouse.matzip.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
+
 @Entity
+@Getter
 public class Board {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long id;
+    private long id;
 
-    private String title;
-    private String contents;
-    private int likes;
-    private LocalDateTime writeDt;
-    private LocalDateTime editDt;
+    // 제목
+    @Setter
+    private String title = "";
+
+    // 내용
+    @Setter
+    private String contents = "";
+
+    // 좋아요 수
+    private int likes = 0;
+
+    // 생성 시간
+    private LocalDateTime createDt = LocalDateTime.now();
+
+    // 수정 시간
+    private LocalDateTime editDt = LocalDateTime.now();
+
+    // 엔티티가 수정될 때 자동으로 갱신
+    @PreUpdate
+    protected void onUpdate() {
+        this.editDt = LocalDateTime.now();
+    }
+
+    public void addLikes(){ this.likes++; }
+
+    public void minusLikes(){ this.likes--; }
+
+    // 위도
+    @Setter
+    private double latitude;
+
+    // 경도
+    @Setter
+    private double longitude;
 }
