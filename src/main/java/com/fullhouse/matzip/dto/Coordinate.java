ad80e1b39837d5f05f4bbbebba4c1842d3e8ec7d
chopin-Op-23 <137890694+chopin-Op-23@users.noreply.github.com>
2024-06-03 23:30:00 +0900

    Add files via upload

---
+++ src/main/java/com/fullhouse/matzip/dto/Coordinate.java
@@ -0,0 +1,16 @@
+package com.fullhouse.matzip.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coordinate {
+    @Schema(description = "위도")
+    private double latitude;
+
+    @Schema(description = "경도")
+    private double longitude;
+
+}
