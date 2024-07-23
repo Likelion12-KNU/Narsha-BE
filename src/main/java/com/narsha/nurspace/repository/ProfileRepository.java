e51bcb4f8f5935e0b191b1c52d2c4c1e906354b6
chopin-Op-23 <137890694+chopin-Op-23@users.noreply.github.com>
2024-07-23 14:27:28 +0900

    Add files via upload

---
+++ src/main/java/com/narsha/nurspace/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.narsha.nurspace.repository;
+
+import com.narsha.nurspace.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+}
